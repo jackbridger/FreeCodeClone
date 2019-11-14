007431a55774a58a553b9a3a8fd5339a3adcfc3d
Jack Bridger <jacksbridger@gmail.com>
2019-11-14 16:24:36 +0900

    front end communicating with backend

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = React.useState('you')
+  React.useEffect(() => {
+    fetch('http://localhost:3000/api/freecodeclones?access_token=5dcd00b2546d7d5f7d84c345').then(res =>
+      res.json()).then(res => setLoggedInUser(res[1].username))
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Welcome to FreeCodeClone, {loggedInUser}</h3>
+
     </div>
   );
 }
