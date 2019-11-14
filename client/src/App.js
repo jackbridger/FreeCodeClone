22081bda28173861e352c873bc60583942ad0fbf
Jack Bridger <jacksbridger@gmail.com>
2019-11-14 18:10:48 +0900

    post req from frontend to backend

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [loggedInUser, setLoggedInUser] = React.useState('you')
-  React.useEffect(() => {
-    fetch('http://localhost:3000/api/freecodeclones?access_token=5dcd00b2546d7d5f7d84c345').then(res =>
-      res.json()).then(res => setLoggedInUser(res[1].username))
-  })
+  const [loggedInUser, setLoggedInUser] = React.useState('')
+  const [password, setPassword] = React.useState('')
+
+
+  const handleLogInSubmit = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:3000/api/Users', {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify({
+        "username": "stringsdsd3",
+        "email": "string3434@g.com",
+        "emailVerified": false,
+        "password": "helflf"
+      }) // body data type must match "Content-Type" header
+    }).then(res => res.json()).then(res => console.log(res))
+  }
 
   return (
     <div className="App">
       <h3>Welcome to FreeCodeClone, {loggedInUser}</h3>
+      <form onSubmit={e => handleLogInSubmit(e)}>
+        username
+        <input type="text" value={loggedInUser} onChange={e => setLoggedInUser(e.target.value)} />
+        Password
+        <input type="text" value={password} onChange={e => setPassword(e.target.value)} />
+        <input type="submit" />
+      </form>
 
     </div>
   );
